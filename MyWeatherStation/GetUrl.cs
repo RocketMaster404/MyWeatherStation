7b743fbed8f66076ac4e0ae3d463b672c81ef38d
erik ny <erik.nyy@gmail.com>
2026-01-11 13:55:41 +0100

    Add project files.

---
+++ MyWeatherStation/GetUrl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWeatherStation
+{
+    internal class GetUrl
+    {
+        public string SmhiUrl(double longitude,double latitude)
+        {
+            string longitudeString = longitude.ToString();
+            string latitude
+        }
+    }
+}
