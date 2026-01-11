ce75aa7484b113b6bfd486b548ed559ecb3d473f
erik ny <erik.nyy@gmail.com>
2026-01-11 14:06:50 +0100

    Created method to collect weather data from SMHI API

--- MyWeatherStation/GetUrl.cs
+++ MyWeatherStation/GetUrl.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,10 +9,40 @@ namespace MyWeatherStation
 {
     internal class GetUrl
     {
-        public string SmhiUrl(double longitude,double latitude)
+        public string SmhiUrl(double longitude, double latitude)
         {
             string longitudeString = longitude.ToString();
-            string latitude
+            string latitudeString = latitude.ToString();
+
+            if (longitudeString.Contains(","))
+            {
+                longitudeString = longitudeString.Replace(",", ".");
+            }
+
+            if (latitudeString.Contains(","))
+            {
+                latitudeString = latitudeString.Replace(",", ".");
+
+            }
+
+            string url = $"https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/{longitudeString}/lat/{latitudeString}/data.json";
+            return url;
+        }
+
+        public string WeatherFromStream(double longitude,double latitude)
+        {
+            string url = SmhiUrl(longitude, latitude);
+
+            var webClient = new WebClient();
+
+            using (Stream file = webClient.OpenRead(url))
+            {
+                using (StreamReader reader = new StreamReader(file))
+                {
+                    string smhiJson = reader.ReadToEnd();
+                    return smhiJson;
+                }
+            }
         }
     }
 }
